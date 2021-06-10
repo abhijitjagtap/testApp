e65f9c16c16b63436dafcdd44698372f2bc68526
j000-name <abhijit.jagtap@simreka.com>
2021-06-10 15:21:55 +0530

    code changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel1
+                    Laravel1234
                 </div>
 
                 <div class="links">
